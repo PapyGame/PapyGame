6205ee12cdc3ad1ece5a5f38771b85753845dcc9
Lory1403 <67193256+Lory1403@users.noreply.github.com>
2024-08-16 18:12:49 +0200

    Modified existingGradeAPI

--- src/main/java/eu/fbk/PapyGame/controller/ApiController.java
+++ src/main/java/eu/fbk/PapyGame/controller/ApiController.java
@@ -173,7 +173,7 @@ public class ApiController {
         }
         String previousResults = project.getGraderResults();
         if (previousResults.equals("")) {
-            return ResponseEntity.badRequest().body("");
+            return ResponseEntity.ok().body("");
         } else {
             return ResponseEntity.ok(jsonFormatterService.format(previousResults));
         }
